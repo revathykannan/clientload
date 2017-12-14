3923e38b6125b8501198621dc4e971ebbd2167f8
revathykannan <revathy.kannan26@gmail.com>
2017-12-14 11:18:32 +0530

    errors

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(_dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8000);
 
 app.get('/*', function(req, res){
     res.sendfile(path.join(__dirname + '/public/index.html'));
